99d1f1b564f070b8143bda19dd7dc5b0dd57b9e9
MorrisLin <aka611511@gmail.com>
2024-07-27 13:51:41 +0800

    feat: split health api to v1 & v2

--- src/main/java/com/coffee/pos/controller/HealthCheckController.java
+++ src/main/java/com/coffee/pos/controller/HealthCheckController.java
@@ -15,8 +15,13 @@ public class HealthCheckController {
      * DeleteMapping
      */
 
-    @GetMapping("/health")
-    public String healthCheck() {
-        return "Application is running";
+    @GetMapping("api/v1/health")
+    public String healthCheckV1() {
+        return "Application v1 is running";
+    }
+
+    @GetMapping("api/v2/health")
+    public String healthCheckV2() {
+        return "Application v2 is running";
     }
 }
