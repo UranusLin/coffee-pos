c2c210d732be6a83606f1cbb78205fbe2e7c8df2
MorrisLin <aka611511@gmail.com>
2024-07-27 15:00:40 +0800

    feat: modify health v2 to return custom response

--- src/main/java/com/coffee/pos/controller/HealthCheckController.java
+++ src/main/java/com/coffee/pos/controller/HealthCheckController.java
@@ -2,6 +2,9 @@ package com.coffee.pos.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import com.coffee.pos.dto.CommonResponse;
 
 
 @RestController
@@ -21,7 +24,10 @@ public class HealthCheckController {
     }
 
     @GetMapping("api/v2/health")
-    public String healthCheckV2() {
-        return "Application v2 is running";
+    public ResponseEntity<CommonResponse> healthCheckV2() {
+        CommonResponse commonResponse = new CommonResponse();
+        commonResponse.setCode("00");
+        commonResponse.setMsg("success");
+        return new ResponseEntity<>(commonResponse, HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/coffee/pos/dto/CommonResponse.java
@@ -0,0 +1,9 @@
+package com.coffee.pos.dto;
+
+import lombok.Data;
+
+@Data
+public class CommonResponse {
+    private String code;
+    private String msg;
+}
